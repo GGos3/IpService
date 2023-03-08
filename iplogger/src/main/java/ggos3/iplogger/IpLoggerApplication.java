203df7cd2ca7b748ff298781ddada2d70c5d5e6b
JangFeel <janpll0421@gmail.com>
2023-03-08 14:01:47 +0900

    CHORE: Spring Init

---
+++ iplogger/src/main/java/ggos3/iplogger/IpLoggerApplication.java
@@ -0,0 +1,13 @@
+package ggos3.iplogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpLoggerApplication.class, args);
+    }
+
+}
---
+++ iplogger/src/test/java/ggos3/iplogger/IploggerApplicationTests.java
@@ -0,0 +1,13 @@
+package ggos3.iplogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IploggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
