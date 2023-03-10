4732211ecece7607cebb258297637ae80b6ab12f
JangFeel <janpll0421@gmail.com>
2023-03-10 10:32:13 +0900

    FEAT: 환경변수 입력을 통한 XFF 헤더 파싱 기능 추가

--- iplogger/src/main/java/ggos3/iplogger/Service/IpParsingService.java
+++ iplogger/src/main/java/ggos3/iplogger/Service/IpParsingService.java
@@ -1,21 +1,27 @@
 package ggos3.iplogger.Service;
 
 import ggos3.iplogger.RequestHeader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@PropertySource("classpath:ProxyForwarded.properties")
 public class IpParsingService implements IpService{
     String xForwardedFor;
     String remoteAddr;
     String ip;
+    @Value("${Forwarded}")
+    private String Forwarded;
 
     @Override
     public Map<String, String> parsingIp(RequestHeader header) {
         Map<String, String> ipMap = new HashMap<>();
         getHeader(header);
+        int Forwarded = getProperty();
 
         if (xForwardedFor == null || xForwardedFor.length() == 0) {
             if(remoteAddr.equals("0:0:0:0:0:0:0:1"))
@@ -26,13 +32,20 @@ public class IpParsingService implements IpService{
         }
 
         String[] ipArray = xForwardedFor.split(",");
-        ip = ipArray[0];
+        int i = (ipArray.length) - Forwarded;
+        ip = ipArray[i];
 
         ipMap.put("Ip", ip);
         ipMap.put("X-Forwarded-For", xForwardedFor);
 
         return ipMap;
     }
+
+    // ProxyForwarded.properties
+    public int getProperty() {
+        return Integer.parseInt(Forwarded);
+    }
+
     public void getHeader(RequestHeader header) {
         this.xForwardedFor = header.getXForwardedFor();
         this.remoteAddr = header.getRemoteAddr();
