18b0a338401aebf0594ec5a0a0265bf59c225e98
GGos3 <janpll0421@gmail.com>
2023-03-11 16:24:14 +0900

    REFACTOR: 전체 파일 경로 변경

--- IpService/src/main/java/ggos3/IpService/Service/IpService.java
+++
@@ -1,9 +0,0 @@
-package ggos3.IpService.Service;
-
-import ggos3.IpService.RequestHeader;
-
-import java.util.Map;
-
-public interface IpService {
-    Map<String, String> parsingIp(RequestHeader header);
-}
